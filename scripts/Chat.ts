71e406b52ed316f0b78d0d635ce73683b379f34d
Marwan Zibaoui <marwan@ourcompany.ch>
2016-07-03 13:33:10 +0200

    chat toggler

--- scripts/Chat.ts
+++ scripts/Chat.ts
@@ -1,8 +1,11 @@
 module Chat {
-    document.getElementById("chat-toggler").addEventListener("click", () => {
-        if(document.body.classList.contains("chat-open"))
-            document.body.classList.remove("chat-open");
-        else
-            document.body.classList.add("chat-open");
-    });
+    var toggler = document.getElementById("chat-toggler");
+    if(toggler) { 
+        toggler.addEventListener("click", () => {
+            if(document.body.classList.contains("chat-open"))
+                document.body.classList.remove("chat-open");
+            else
+                document.body.classList.add("chat-open");
+        });
+    }
 }
--- scripts/js/output.js
+++ scripts/js/output.js
@@ -15,12 +15,15 @@ var Background;
 })(Background || (Background = {}));
 var Chat;
 (function (Chat) {
-    document.getElementById("chat-toggler").addEventListener("click", function () {
-        if (document.body.classList.contains("chat-open"))
-            document.body.classList.remove("chat-open");
-        else
-            document.body.classList.add("chat-open");
-    });
+    var toggler = document.getElementById("chat-toggler");
+    if (toggler) {
+        toggler.addEventListener("click", function () {
+            if (document.body.classList.contains("chat-open"))
+                document.body.classList.remove("chat-open");
+            else
+                document.body.classList.add("chat-open");
+        });
+    }
 })(Chat || (Chat = {}));
 var Helpers;
 (function (Helpers) {
