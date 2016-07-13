af7dfa92565c8dc096ad64c7a6086e7ff6131cc2
Marwan Zibaoui <marwan@ourcompany.ch>
2016-07-14 01:37:20 +0200

    more gifs

--- scripts/Logo.ts
+++ scripts/Logo.ts
@@ -1,7 +1,10 @@
 module Logo {
     var logoElt:HTMLElement = <HTMLElement>document.querySelector(".footer-logo");
     var imgElt: HTMLImageElement = <HTMLImageElement>document.querySelector("img.logo-img");
-    var images = ["assets/banana.gif", "assets/kayne.gif", "assets/pukingdog.gif", "assets/spongebob.gif"];
+    var images = [
+        "assets/banana.gif", "assets/flyingcat.gif", "assets/kayne.gif", "assets/pukingdog.gif", 
+        "assets/catdonut.gif", "assets/nicecat.gif", "assets/spongebob.gif"
+    ];
     var loader = document.createElement("img");
     loader.style.position = "fixed";
     loader.style.left = "-3000px";
