0ce036efb6e5d4705fdc4690ccb59ffee8778542
Marwan Zibaoui <marwan@ourcompany.ch>
2016-08-01 01:44:37 +0200

    minifying

--- scripts/lib/definitions-custom.js
+++
@@ -1,7 +0,0 @@
-var Direction;
-(function (Direction) {
-    Direction[Direction["Top"] = 0] = "Top";
-    Direction[Direction["Right"] = 1] = "Right";
-    Direction[Direction["Bottom"] = 2] = "Bottom";
-    Direction[Direction["Left"] = 3] = "Left";
-})(Direction || (Direction = {}));
